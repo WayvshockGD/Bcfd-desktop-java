0ca14536a8265493b3693bae9c1ad4719fa5032f
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2020-12-31 04:14:15 -0500

    Add files via upload

---
+++ src/bcfd/me/wayv/Main.java
@@ -0,0 +1,15 @@
+package bcfd.me.wayv;
+
+import javax.swing.*;
+
+public class Main {
+    private JPanel MainPanel;
+    private JButton Host;
+    private JButton button1;
+    private JButton button2;
+    private JPasswordField TokenBox;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
